f9ca018a13a0c374b4962eb70ab4b31ff73f1141
Anderson <andersonmendes05@gmail.com>
2022-05-23 09:59:11 -0300

    Initial commit

---
+++ Home.js
@@ -0,0 +1,68 @@
+const publis = ["Imagens/img1.jpg","Imagens/img2.jpg","Imagens/img3.jpg","Imagens/img4.jpg"];
+
+function publicidade(img){
+    document.getElementById('img_carro').src=publis[img];
+    img = img<publis.length-1 ? img+1: 0;
+    let timer = setTimeout(publicidade, 3000, img);
+};
+
+const categorias = [
+    {
+        imagem: "Imagens/cat1.png",
+        nome: "Lâmpadas"
+    },
+    {
+        imagem: "Imagens/cat2.png",
+        nome: "Fios",
+    },
+    {
+        imagem: "Imagens/cat3.png",
+        nome: "Disjuntores",
+    },
+    {
+        imagem: "Imagens/cat4.png",
+        nome: "Contactores",
+    },
+    {
+        imagem: "Imagens/cat5.png",
+        nome: "Capacitores",
+    },
+];
+
+function mostra_cat(inicial) {
+    document.getElementById('img_cat1').src=categorias[inicial].imagem;
+    document.getElementById('txt_cat1').innerText=categorias[inicial].nome;
+    
+    document.getElementById('img_cat2').src=categorias[inicial+1].imagem;
+    document.getElementById('txt_cat2').innerText=categorias[inicial+1].nome;
+    
+    document.getElementById('img_cat3').src=categorias[inicial+2].imagem;
+    document.getElementById('txt_cat3').innerText=categorias[inicial+2].nome;
+
+    document.getElementById('img_cat4').src=categorias[inicial+3].imagem;
+    document.getElementById('txt_cat4').innerText=categorias[inicial+3].nome;
+}
+
+let indice = 0;
+
+function direita(){
+    if(indice<categorias.length-4){
+        indice++;
+    }else{
+        indice=0;
+    }
+    
+    mostra_cat(indice);
+}
+
+function esquerda(){
+    if(indice<=0){
+        indice=categorias.length-4;
+    }else{
+        indice--;
+    }
+    
+    console.log("indice: " + indice + "\ncategoria:" + categorias.length)
+
+    mostra_cat(indice);
+}
